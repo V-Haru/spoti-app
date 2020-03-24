cf377483a35b9560eaecfdc72cc7cfe192b4f898
V-Haru <kronk.sdance@gmail.com>
2020-03-23 23:48:34 -0600

    se creo el pipe que noimage que valida si existe la imagen, ademas de modificar los components de home y search optimizandoos mediante el service y 2 nuevos components, uno de loading y otro de tarjetas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-  token: string
+  token
     = 'Your token';
 
-  constructor(private http: HttpClient) {
-    console.log('listo');
+  constructor(private http: HttpClient) { }
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`
+    });
+
+    return this.http.get(url, { headers });
   }
 
   getNewReleases() {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`
-    });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.getQuery('browse/new-releases?limit=20')
+      .pipe(map((data: any) => data.albums.items
+      ));
   }
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers });
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+      .pipe(map((data: any) => data.artists.items
+      ));
   }
 }
