dee56e0bf82225c0fd527c2d595813b80969430c
V-Haru <kronk.sdance@gmail.com>
2020-03-23 22:31:37 -0600

    Base components home, artist, search, navbar, rutas y service

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+  token: string;
+
+  constructor(private http: HttpClient) {
+    console.log('listo');
+  }
+
+  getNewReleases() {
+    this.token = 'Your token';
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+      .subscribe(data => {
+        console.log(data);
+
+      });
+  }
+}
