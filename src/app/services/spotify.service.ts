3dbbf5c64294aa87c4ce9f84628dfddabcc6b8a6
V-Haru <kronk.sdance@gmail.com>
2020-03-23 23:01:48 -0600

    se añadio la base del search y se modifico la parte del servicio añadiendo el metodo getArtista

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -5,17 +5,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SpotifyService {
-  token: string;
+  token: string
+    = 'Your token';
 
   constructor(private http: HttpClient) {
     console.log('listo');
   }
 
   getNewReleases() {
-    this.token = 'Your token';
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+  }
+
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers });
   }
 }
